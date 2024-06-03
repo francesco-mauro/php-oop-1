1c39defe311fff62e0ba675b1d00a15da9c305e8
francesco-mauro <francesco.mauro180@gmail.com>
2024-06-03 10:14:17 +0200

    add: corrections

--- index.php
+++ index.php
@@ -12,14 +12,14 @@ $movie1 = new Movie("Luca 1", 120, $genere1);
 $movie2 = new Movie("Loris", 90, $genere2);
 
 // ---------------------------------Stampare le proprietà ---------------------------------
-echo "Titolo: " . $movie1->getTitle() . "\n";
-echo "Durata: " . $movie1->getDuration() . " minuti\n";
-echo "Genere: " . $movie1->getGenre()->getName() . "\n";
+echo "Titolo: " . $movie1->getTitle();
+echo "Durata: " . $movie1->getDuration() . " minuti";
+echo "Genere: " . $movie1->getGenre()->getName();
 
 echo "\n";
 
-echo "Titolo: " . $movie2->getTitle() . "\n";
-echo "Durata: " . $movie2->getDuration() . " minuti\n";
-echo "Genere: " . $movie2->getGenre()->getName() . "\n";
+echo "Titolo: " . $movie2->getTitle() ;
+echo "Durata: " . $movie2->getDuration() . " minuti";
+echo "Genere: " . $movie2->getGenre()->getName() ;
 
 ?>
